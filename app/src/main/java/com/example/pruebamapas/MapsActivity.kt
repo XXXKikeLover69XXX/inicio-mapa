4329b5819a46f1f30806cd64e916d703f96ef17c
André <lalbrob.genius@gmail.com>
2025-06-13 09:50:05 +0200

    marker-con-imagen

--- app/src/main/java/com/example/pruebamapas/MapsActivity.kt
+++ app/src/main/java/com/example/pruebamapas/MapsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.pruebamapas
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
@@ -9,9 +12,11 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import androidx.core.graphics.scale
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -37,30 +42,42 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(santiago, 13f))
 
         val cersia = LatLng(42.885379650416255, -8.516780818558765)
+        val iconoajustado = resizeMapIcon(R.drawable.sabroso, 100, 100)
         markerCersia = mMap.addMarker(
             MarkerOptions()
                 .position(cersia)
                 .title("Edificio CERSIA")
-
+                .icon(iconoajustado)
         )!!
 
         mMap.setOnMarkerClickListener { marker ->
             if (marker == markerCersia) {
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(cersia, 17f))
-                btnNavegar.visibility = Button.VISIBLE
+                btnNavegar.visibility = View.VISIBLE
                 true
             } else {
                 false
             }
         }
         mMap.setOnMapClickListener {
-            btnNavegar.visibility = Button.GONE
+            btnNavegar.visibility = View.GONE
         }
         btnNavegar.setOnClickListener {
-            val uri = "https://www.google.com/maps/dir/?api=1&destination=42.858710,-8.562530".toUri()
+            val uri =
+                "https://www.google.com/maps/dir/?api=1&destination=42.858710,-8.562530".toUri()
             val intent = Intent(Intent.ACTION_VIEW, uri)
             intent.setPackage("com.google.android.apps.maps")
             startActivity(intent)
         }
+    }
+
+    private fun resizeMapIcon(
+        iconResId: Int,
+        width: Int,
+        height: Int
+    ): com.google.android.gms.maps.model.BitmapDescriptor {
+        val imageBitmap = BitmapFactory.decodeResource(applicationContext.resources, iconResId)
+        val resizedBitmap = imageBitmap.scale(width, height)
+        return BitmapDescriptorFactory.fromBitmap(resizedBitmap)
     }
 }
